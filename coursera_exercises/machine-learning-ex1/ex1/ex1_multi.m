da08e9846e713559eb1f068833cde6c092dddf2f
Joey Huang <kamidox@qq.com>
2015-09-22 00:20:52 +0800

    coursera ml ex1 multi gradient descent

--- coursera_exercises/machine-learning-ex1/ex1/ex1_multi.m
+++ coursera_exercises/machine-learning-ex1/ex1/ex1_multi.m
@@ -3,11 +3,11 @@
 %
 %  Instructions
 %  ------------
-% 
-%  This file contains code that helps you get started on the
-%  linear regression exercise. 
 %
-%  You will need to complete the following functions in this 
+%  This file contains code that helps you get started on the
+%  linear regression exercise.
+%
+%  You will need to complete the following functions in this
 %  exericse:
 %
 %     warmUpExercise.m
@@ -51,6 +51,15 @@ fprintf('Normalizing Features ...\n');
 
 [X mu sigma] = featureNormalize(X);
 
+% Print out some data points
+% fprintf('First 10 normalized examples from the dataset: \n');
+% fprintf(' x = [%.04f %.04f], y = %.0f \n', [X(1:10,:) y(1:10,:)]');
+% fprintf(' mu = [%.04f %.04f] \n', mu);
+% fprintf(' sigma = [%.04f %.04f] \n', sigma);
+%
+% fprintf('Program paused. Press enter to continue.\n');
+% pause;
+
 % Add intercept term to X
 X = [ones(m, 1) X];
 
@@ -60,13 +69,13 @@ X = [ones(m, 1) X];
 % ====================== YOUR CODE HERE ======================
 % Instructions: We have provided you with the following starter
 %               code that runs gradient descent with a particular
-%               learning rate (alpha). 
+%               learning rate (alpha).
 %
-%               Your task is to first make sure that your functions - 
-%               computeCost and gradientDescent already work with 
+%               Your task is to first make sure that your functions -
+%               computeCost and gradientDescent already work with
 %               this starter code and support multiple variables.
 %
-%               After that, try running gradient descent with 
+%               After that, try running gradient descent with
 %               different values of alpha and see which one gives
 %               you the best result.
 %
@@ -85,7 +94,7 @@ fprintf('Running gradient descent ...\n');
 alpha = 0.01;
 num_iters = 400;
 
-% Init Theta and Run Gradient Descent 
+% Init Theta and Run Gradient Descent
 theta = zeros(3, 1);
 [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters);
 
@@ -104,7 +113,7 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = 0; % You should change this
+price = [1 (1650 - mu(1)) / sigma(1) (3 - mu(2)) / sigma(2)] * theta;
 
 
 % ============================================================
@@ -112,6 +121,7 @@ price = 0; % You should change this
 fprintf(['Predicted price of a 1650 sq-ft, 3 br house ' ...
          '(using gradient descent):\n $%f\n'], price);
 
+% save 'multi_gd.mat';    % save state for debug and compare
 fprintf('Program paused. Press enter to continue.\n');
 pause;
 
@@ -120,12 +130,12 @@ pause;
 fprintf('Solving with normal equations...\n');
 
 % ====================== YOUR CODE HERE ======================
-% Instructions: The following code computes the closed form 
+% Instructions: The following code computes the closed form
 %               solution for linear regression using the normal
-%               equations. You should complete the code in 
+%               equations. You should complete the code in
 %               normalEqn.m
 %
-%               After doing so, you should complete this code 
+%               After doing so, you should complete this code
 %               to predict the price of a 1650 sq-ft, 3 br house.
 %
 
