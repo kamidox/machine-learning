0ce3193362e6abae9a5a7d5086fdc2cca87ec545
Joey Huang <kamidox@qq.com>
2015-09-22 21:59:50 +0800

    coursera ml ex1: normal equaltions

--- coursera_exercises/machine-learning-ex1/ex1/ex1_multi.m
+++ coursera_exercises/machine-learning-ex1/ex1/ex1_multi.m
@@ -132,15 +132,14 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = [1 (1650 - mu(1)) / sigma(1) (3 - mu(2)) / sigma(2)] * theta;
 
+price = [1 (1650 - mu(1)) / sigma(1) (3 - mu(2)) / sigma(2)] * theta;
+% save 'multi_gd.mat';    % save state for debug and compare
 
 % ============================================================
 
 fprintf(['Predicted price of a 1650 sq-ft, 3 br house ' ...
          '(using gradient descent):\n $%f\n'], price);
-
-% save 'multi_gd.mat';    % save state for debug and compare
 fprintf('Program paused. Press enter to continue.\n');
 pause;
 
@@ -178,8 +177,8 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
 
+price = [1 1650 3] * theta;
 
 % ============================================================
 
