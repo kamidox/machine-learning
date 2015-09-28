65d583feb41c89fc980367e0f296bfb514d1c570
Joey Huang <kamidox@qq.com>
2015-09-28 23:30:32 +0800

    coursera ml ex2 part1

--- coursera_exercises/machine-learning-ex2/ex2/plotData.m
+++ coursera_exercises/machine-learning-ex2/ex2/plotData.m
@@ -1,5 +1,5 @@
 function plotData(X, y)
-%PLOTDATA Plots the data points X and y into a new figure 
+%PLOTDATA Plots the data points X and y into a new figure
 %   PLOTDATA(x,y) plots the data points with + for the positive examples
 %   and o for the negative examples. X is assumed to be a Mx2 matrix.
 
@@ -12,16 +12,13 @@ figure; hold on;
 %               examples and 'ko' for the negative examples.
 %
 
+positive = find(y == 1);
+negative = find(y == 0);
 
-
-
-
-
-
-
+plot(X(positive, 1), X(positive, 2), 'k+', 'LineWidth', 2, 'MarkerSize', 5);
+plot(X(negative, 1), X(negative, 2), 'ko', 'MarkerFaceColor', 'y', 'MarkerSize', 5);
 
 % =========================================================================
-
 
 
 hold off;
--- coursera_exercises/machine-learning-ex2/ex2/sigmoid.m
+++ coursera_exercises/machine-learning-ex2/ex2/sigmoid.m
@@ -2,17 +2,22 @@ function g = sigmoid(z)
 %SIGMOID Compute sigmoid functoon
 %   J = SIGMOID(z) computes the sigmoid of z.
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 g = zeros(size(z));
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the sigmoid of each value of z (z can be a matrix,
 %               vector or scalar).
 
+% LIMITATIONS: The dimensions of matrix should not large than 2, or this will not work
 
+xdim = size(z, 1);
+ydim = size(z, 2);
 
-
-
+for i = 1:xdim
+    for j = 1:ydim
+        g(i, j) = (1 + e ** (-z(i, j))) ** (-1);
+    end
 % =============================================================
 
 end
