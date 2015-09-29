d5e3231ca86f780547ef212171b0b0f54c1e9f37
Joey Huang <kamidox@qq.com>
2015-09-29 21:55:11 +0800

    coursera ml ex2 comments

--- coursera_exercises/machine-learning-ex2/ex2/plotDecisionBoundary.m
+++ coursera_exercises/machine-learning-ex2/ex2/plotDecisionBoundary.m
@@ -1,10 +1,10 @@
 function plotDecisionBoundary(theta, X, y)
 %PLOTDECISIONBOUNDARY Plots the data points X and y into a new figure with
 %the decision boundary defined by theta
-%   PLOTDECISIONBOUNDARY(theta, X,y) plots the data points with + for the 
-%   positive examples and o for the negative examples. X is assumed to be 
-%   a either 
-%   1) Mx3 matrix, where the first column is an all-ones column for the 
+%   PLOTDECISIONBOUNDARY(theta, X,y) plots the data points with + for the
+%   positive examples and o for the negative examples. X is assumed to be
+%   a either
+%   1) Mx3 matrix, where the first column is an all-ones column for the
 %      intercept.
 %   2) MxN, N>3 matrix, where the first column is all-ones
 
@@ -17,11 +17,19 @@ if size(X, 2) <= 3
     plot_x = [min(X(:,2))-2,  max(X(:,2))+2];
 
     % Calculate the decision boundary line
+    % REMARK: prove the the plot_y calculators
+    % let sigmoid(z) = 0.5 be the decision boundary
+    % => e^{-z} = 1
+    % => z = 0
+    % => X * theta = 0
+    % => x_0 * theta_0 + x_1 * theta_1 + x_2 * theta_2 = 0
+    % => x_2 = - theta_2 * (x_1 * theta_1 + x_0 * theta_0)
+    % => x_2 = - theta_2 * (x_1 * theta_1 + theta_0)
     plot_y = (-1./theta(3)).*(theta(2).*plot_x + theta(1));
 
     % Plot, and adjust axes for better viewing
     plot(plot_x, plot_y)
-    
+
     % Legend, specific for the exercise
     legend('Admitted', 'Not admitted', 'Decision Boundary')
     axis([30, 100, 30, 100])
@@ -39,7 +47,8 @@ else
     end
     z = z'; % important to transpose z before calling contour
 
-    % Plot z = 0
+    % Plot z = 0; let sigmoid(z) = 0.5 be the decision boundary => z = 0
+    % Plot contour of X * theta = 0
     % Notice you need to specify the range [0, 0]
     contour(u, v, z, [0, 0], 'LineWidth', 2)
 end
