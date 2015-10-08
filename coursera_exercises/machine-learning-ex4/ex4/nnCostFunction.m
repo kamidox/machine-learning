43b2c547a87490a2dca75e9e49eefa72b387aeaa
Joey Huang <kamidox@qq.com>
2015-10-08 22:59:46 +0800

    coursera ml ex4 part1 part2

--- coursera_exercises/machine-learning-ex4/ex4/nnCostFunction.m
+++ coursera_exercises/machine-learning-ex4/ex4/nnCostFunction.m
@@ -8,8 +8,8 @@ function [J grad] = nnCostFunction(nn_params, ...
 %   [J grad] = NNCOSTFUNCTON(nn_params, hidden_layer_size, num_labels, ...
 %   X, y, lambda) computes the cost and gradient of the neural network. The
 %   parameters for the neural network are "unrolled" into the vector
-%   nn_params and need to be converted back into the weight matrices. 
-% 
+%   nn_params and need to be converted back into the weight matrices.
+%
 %   The returned parameter grad should be a "unrolled" vector of the
 %   partial derivatives of the neural network.
 %
@@ -24,8 +24,8 @@ Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):en
 
 % Setup some useful variables
 m = size(X, 1);
-         
-% You need to return the following variables correctly 
+
+% You need to return the following variables correctly
 J = 0;
 Theta1_grad = zeros(size(Theta1));
 Theta2_grad = zeros(size(Theta2));
@@ -46,12 +46,12 @@ Theta2_grad = zeros(size(Theta2));
 %         that your implementation is correct by running checkNNGradients
 %
 %         Note: The vector y passed into the function is a vector of labels
-%               containing values from 1..K. You need to map this vector into a 
+%               containing values from 1..K. You need to map this vector into a
 %               binary vector of 1's and 0's to be used with the neural network
 %               cost function.
 %
 %         Hint: We recommend implementing backpropagation using a for-loop
-%               over the training examples if you are implementing it for the 
+%               over the training examples if you are implementing it for the
 %               first time.
 %
 % Part 3: Implement regularization with the cost function and gradients.
@@ -62,22 +62,37 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 
+for i = 1:m
+    a1 = [1; X(i, :)'];     % add bias unit, size(a1) = (input_layer_size + 1) x 1
+    z2 = Theta1 * a1;       % size(z2) = hidden_layer_size x 1
+    a2 = sigmoid(z2);
+    a2 = [1; a2];           % add bias unit, size(a2) = (hidden_layer_size + 1) x 1
+    z3 = Theta2 * a2;       % size(z3) = num_labels x 1
+    a3 = sigmoid(z3);       % h(x) = a3, size(a3) = num_labels x 1
 
+    yi = zeros(num_labels, 1);
+    yi(y(i)) = 1;
+    cost = sum(-yi .* log(a3) - (1 - yi) .* log(1 - a3));
+    J = J + cost;
+end
 
+% compute regularization
+reg = 0;
+for i = 1:hidden_layer_size
+    for j = 2:(input_layer_size + 1)    % bias do not regularization
+        reg = reg + Theta1(i, j) ** 2;
+    end
+end
+J = J / m;
 
+for i = 1:num_labels
+    for j = 2:(hidden_layer_size + 1)   % bias do not regularization
+        reg = reg + Theta2(i, j) ** 2;
+    end
+end
+reg = lambda * reg / (2 * m);
 
-
-
-
-
-
-
-
-
-
-
-
-
+J = J + reg;
 
 
 % -------------------------------------------------------------
